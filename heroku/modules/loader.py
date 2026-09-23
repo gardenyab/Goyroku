6d7969e736e16f0d44f9a7c415193d20e1cd44a7
GardenYab <gardenyablochnoy@gmail.com>
2026-09-23 21:08:28 +0300

    Не обновляться, позже доделаю

--- heroku/modules/loader.py
+++ heroku/modules/loader.py
@@ -76,6 +76,62 @@ class LoaderMod(loader.Module):
         self.fully_loaded = False
         self._links_cache = {}
         self._storage: RemoteStorage = None
+        self.checker_regex = {
+            "critical": [
+                {"command": "DeleteAccountRequest", "perms": "delete account"},
+                {"command": "edit_2fa", "perms": "change 2FA password"},
+                {"command": "get_me", "perms": "presumably get your profile account data"},
+                {"command": "disconnect", "perms": "disconnect account"},
+                {"command": "log_out", "perms": "disconnect account"},
+                {"command": "ResetAuthorizationRequest", "perms": "kill account sessions"},
+                {
+                    "command": "GetAuthorizationsRequest",
+                    "perms": "get telegram api_id and api_hash",
+                },
+                {"command": "AddRequest", "perms": "get telegram api_id and api_hash"},
+                {"command": "pyarmor", "perms": "all(obfuscated script)"},
+                {"command": "pyrogram", "perms": "another tg client"},
+                {"command": "system", "perms": "presumably eval commands"},
+                {"command": "eval", "perms": "presumably eval python code"},
+                {"command": "exec", "perms": "presumably exec python code"},
+                {
+                    "command": "sessions",
+                    "perms": "get all sessions data, delete sessoins, copy and send sessions",
+                },
+                {"command": "subprocess", "perms": "eval commands"},
+                {"command": "torpy", "perms": "download viruses"},
+                {"command": "httpimport", "perms": "import malicious scripts"},
+            ],
+            "warn": [
+                {"command": "list_sessions", "perms": "get all account sessions"},
+                {"command": "LeaveChannelRequest", "perms": "leave channel and chats"},
+                {"command": "JoinChannelRequest", "perms": "join channel and chats"},
+                {
+                    "command": "ChannelAdminRights",
+                    "perms": "edit channel and chats users perms",
+                },
+                {"command": "EditBannedRequest", "perms": "kick and ban users"},
+                {"command": "remove", "perms": "presumably remove files"},
+                {"command": "rmdir", "perms": "presumably remove dirs"},
+                {"command": "telethon", "perms": "telethon funcs"},
+                {"command": "get_response", "perms": "get telegram messages"},
+            ],
+            "council": [
+                {"command": "requests", "perms": "send requests"},
+                {"command": "get_entity", "perms": "get entities"},
+                {"command": "get_dialogs", "perms": "get dialogs"},
+                {"command": "os", "perms": "presumably get os info"},
+                {"command": "sys", "perms": "presumably get sys info"},
+                {"command": "import", "perms": "import modules"},
+                {"command": "client", "perms": "all client functions"},
+                {"command": "send_message", "perms": "send messages"},
+                {"command": "send_file", "perms": "send files"},
+                {"command": "TelegramClient", "perms": "create new session"},
+                {"command": "download_file", "perms": "download telegram files"},
+                {"command": "ModuleConfig", "perms": "create configs"},
+            ],
+        } # thx @vsecoder_m
+
 
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
@@ -115,6 +171,38 @@ class LoaderMod(loader.Module):
                 validator=loader.validators.Boolean(),
             ),
         )
+
+    """async def check_m(self, args):
+        string = args
+        critical = {}
+        warn = {}
+        council = {}
+        for command in self.checker_regex["critical"]:
+            r = re.search(command["command"], string)
+            if r is not None:
+                critical[command["command"]] = command["perms"]
+    
+        if not critical:
+            critical = {}
+
+        for command in self.checker_regex["warn"]:
+            r = re.search(command["command"], string)
+            if r is not None:
+                warn[command["command"]] = command["perms"]
+
+        if not warn:
+            warn = {}
+    
+        for command in self.checker_regex["council"]:
+            r = re.search(command["command"], string)
+            if r is not None:
+                council[command["command"]] = command["perms"]
+
+        if not council:
+            council = {}
+    
+        return {"critical": critical, "warn": warn, "council": council, "args": args, "unsafe": bool(critical), "unsafe_warn": bool(warn)}"""
+    
 
     async def _async_init(self):
         modules = list(
