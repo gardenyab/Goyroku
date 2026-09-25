2c71aa947e2a869d1b807cb1e402bdc0f0677b0f
GardenYab <gardenyablochnoy@gmail.com>
2026-09-25 20:33:19 +0300

    sad

--- heroku/version.py
+++ heroku/version.py
@@ -12,7 +12,7 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (2, 2, 5)
+__version__ = (2, 2, 6)
 
 import os
 
